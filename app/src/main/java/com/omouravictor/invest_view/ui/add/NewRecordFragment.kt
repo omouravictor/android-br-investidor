c45eb954a69af0d8e8bbf28703261ca295b86a29
omouravictor <victor.moura@aluno.ufop.edu.br>
2023-09-03 22:34:14 -0300

    save

--- app/src/main/java/com/omouravictor/invest_view/ui/add/NewRecordFragment.kt
+++ app/src/main/java/com/omouravictor/invest_view/ui/add/NewRecordFragment.kt
@@ -1,4 +1,4 @@
-package com.omouravictor.invest_view.ui.new_asset
+package com.omouravictor.invest_view.ui.add
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,26 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import com.omouravictor.invest_view.databinding.FragmentNewAssetBinding
+import com.omouravictor.invest_view.databinding.FragmentNewRecordBinding
 
-class NewAssetFragment : Fragment() {
+class NewRecordFragment : Fragment() {
 
-    private var _binding: FragmentNewAssetBinding? = null
+    private var _binding: FragmentNewRecordBinding? = null
     private val binding get() = _binding!!
-    private val newAssetViewModel: NewAssetViewModel by viewModels()
+    private val newRecordViewModel: NewRecordViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentNewAssetBinding.inflate(inflater, container, false)
+        _binding = FragmentNewRecordBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        newAssetViewModel.text.observe(viewLifecycleOwner) {
+        newRecordViewModel.text.observe(viewLifecycleOwner) {
         }
     }
 
--- app/src/main/java/com/omouravictor/invest_view/ui/add/NewRecordViewModel.kt
+++ app/src/main/java/com/omouravictor/invest_view/ui/add/NewRecordViewModel.kt
@@ -1,12 +1,12 @@
-package com.omouravictor.invest_view.ui.new_asset
+package com.omouravictor.invest_view.ui.add
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NewAssetViewModel : ViewModel() {
+class NewRecordViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is new assets Fragment"
+        value = "This is new record Fragment"
     }
     val text: LiveData<String> = _text
 }
