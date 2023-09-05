a169329211769e5495c2e3dc9530f2fd9da407c1
omouravictor <victor.moura@aluno.ufop.edu.br>
2023-09-05 15:42:35 -0300

    save

--- app/src/main/java/com/omouravictor/invest_view/MainActivity.kt
+++ app/src/main/java/com/omouravictor/invest_view/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.omouravictor.invest_view
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -14,12 +14,12 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 import com.omouravictor.invest_view.databinding.ActivityMainBinding
-import com.omouravictor.invest_view.ui.new_record.NewRecordActivity
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() {
         val navView: NavigationView = binding.navView
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
         appBarConfiguration = AppBarConfiguration(
             setOf(R.id.nav_fragment_wallet),
@@ -50,7 +50,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.add_option -> {
-                startActivity(Intent(this, NewRecordActivity::class.java))
+                navController.navigate(R.id.action_nav_to_activity_new_record)
             }
         }
 
