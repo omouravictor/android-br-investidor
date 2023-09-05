0a9ffbb015b1d3f28086f58ea0db87ed7729c9ac
omouravictor <victor.moura@aluno.ufop.edu.br>
2023-09-05 00:50:50 -0300

    save

--- app/src/main/java/com/omouravictor/invest_view/ui/new_record/NewRecordActivity.kt
+++ app/src/main/java/com/omouravictor/invest_view/ui/new_record/NewRecordActivity.kt
@@ -2,13 +2,17 @@ package com.omouravictor.invest_view.ui.new_record
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.omouravictor.invest_view.R
 import com.omouravictor.invest_view.databinding.ActivityNewRecordBinding
 
 class NewRecordActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityNewRecordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,8 +23,13 @@ class NewRecordActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_new_record) as NavHostFragment
         val navController = navHostFragment.navController
-        navController.setGraph(R.navigation.new_record_nav)
 
-        setupActionBarWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_new_record)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
