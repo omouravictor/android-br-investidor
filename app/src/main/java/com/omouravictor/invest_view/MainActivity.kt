917ba7a3168e4bdc3fb7095d5194e39cb50bf721
omouravictor <omouravictor@gmail.com>
2024-02-04 14:47:36 -0300

    cria onNavDestinationSelected function igual ao origirinal do NavigationUI.Kt

--- app/src/main/java/com/omouravictor/invest_view/MainActivity.kt
+++ app/src/main/java/com/omouravictor/invest_view/MainActivity.kt
@@ -5,7 +5,10 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ActivityNavigator
 import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
@@ -45,7 +48,51 @@ class MainActivity : AppCompatActivity() {
 
         binding.toolbar.setupWithNavController(navController, appBarConfiguration)
         binding.bottomNav.setupWithNavController(navController)
+
+        binding.bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.walletNavMenu -> {
+                    onNavDestinationSelected(menuItem)
+                    true
+                }
+
+                R.id.exchangeNavMenu -> {
+                    onNavDestinationSelected(menuItem)
+                    true
+                }
+
+                else -> false
+            }
+        }
+
         supportActionBar?.title = navController.currentDestination?.label
+    }
+
+    private fun onNavDestinationSelected(item: MenuItem) {
+        val builder = NavOptions.Builder().setLaunchSingleTop(true).setRestoreState(true)
+        if (
+            navController.currentDestination!!.parent!!.findNode(item.itemId)
+                    is ActivityNavigator.Destination
+        ) {
+            builder.setEnterAnim(androidx.navigation.ui.R.anim.nav_default_enter_anim)
+                .setExitAnim(androidx.navigation.ui.R.anim.nav_default_exit_anim)
+                .setPopEnterAnim(androidx.navigation.ui.R.anim.nav_default_pop_enter_anim)
+                .setPopExitAnim(androidx.navigation.ui.R.anim.nav_default_pop_exit_anim)
+        } else {
+            builder.setEnterAnim(androidx.navigation.ui.R.animator.nav_default_enter_anim)
+                .setExitAnim(androidx.navigation.ui.R.animator.nav_default_exit_anim)
+                .setPopEnterAnim(androidx.navigation.ui.R.animator.nav_default_pop_enter_anim)
+                .setPopExitAnim(androidx.navigation.ui.R.animator.nav_default_pop_exit_anim)
+        }
+        if (item.order and Menu.CATEGORY_SECONDARY == 0) {
+            builder.setPopUpTo(
+                navController.graph.findStartDestination().id,
+                inclusive = false,
+                saveState = true
+            )
+        }
+        val options = builder.build()
+        navController.navigate(item.itemId, null, options)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
