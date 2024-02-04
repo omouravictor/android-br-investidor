8c4cfa00acd8e5bc67afa212079719251d9e6ec7
omouravictor <omouravictor@gmail.com>
2024-02-04 16:23:24 -0300

    refatora layout dos ícones

--- app/src/main/java/com/omouravictor/invest_view/MainActivity.kt
+++ app/src/main/java/com/omouravictor/invest_view/MainActivity.kt
@@ -23,6 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        binding.bottomNav.itemIconTintList = null
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_content_main) as NavHostFragment
