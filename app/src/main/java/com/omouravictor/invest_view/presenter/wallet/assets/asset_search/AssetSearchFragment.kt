8a36924c7b012753cabf49cf381dc75e2d5c84cb
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-03-14 22:13:00 -0300

    continues working on search of assets feature

--- app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/asset_search/AssetSearchFragment.kt
+++ app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/asset_search/AssetSearchFragment.kt
@@ -4,18 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.omouravictor.invest_view.databinding.FragmentAssetSearchBinding
+import com.omouravictor.invest_view.presenter.base.UiResultState
+import com.omouravictor.invest_view.presenter.wallet.assets.asset_search.model.AssetBySearchUiModel
 
 class AssetSearchFragment : Fragment() {
 
     private var _binding: FragmentAssetSearchBinding? = null
     private val binding get() = _binding!!
     private val assetSearchViewModel: AssetSearchViewModel by activityViewModels()
-    private val assetTypeUiArg by lazy {
-        AssetSearchFragmentArgs.fromBundle(requireArguments()).assetTypeUi
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,24 +28,45 @@ class AssetSearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupAssetSearchView()
+        observeAssetSearchResults()
+    }
 
+    private fun setupAssetSearchView() {
         binding.svAssetSearch.postDelayed({ binding.svAssetSearch.onActionViewExpanded() }, 50)
-        binding.svAssetSearch.setOnQueryTextListener(object :
-            androidx.appcompat.widget.SearchView.OnQueryTextListener {
 
+        binding.svAssetSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
                 query?.let { assetSearchViewModel.getAssetsBySearch(it) }
                 return true
             }
 
             override fun onQueryTextChange(newText: String?): Boolean {
-                newText.let {
-                    if (it.isNullOrEmpty())
-                        binding.recyclerView.scrollToPosition(0)
-                }
+                if (newText.isNullOrEmpty())
+                    binding.recyclerView.scrollToPosition(0)
                 return true
             }
         })
+    }
+
+    private fun observeAssetSearchResults() {
+        assetSearchViewModel.assetsBySearch.observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is UiResultState.Success -> handleUiResultStateSuccess(result.data)
+                is UiResultState.Error -> handleUiResultStateError(result.message)
+                is UiResultState.Loading -> handleUiResultStateLoading()
+            }
+        }
+    }
+
+    private fun handleUiResultStateSuccess(data: List<AssetBySearchUiModel>) {
+    }
+
+    private fun handleUiResultStateError(message: String) {
+
+    }
+
+    private fun handleUiResultStateLoading() {
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/asset_search/AssetSearchViewModel.kt
+++ app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/asset_search/AssetSearchViewModel.kt
@@ -6,8 +6,11 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.omouravictor.invest_view.data.network.base.NetworkResultState
 import com.omouravictor.invest_view.data.network.remote.model.assetsbysearch.AssetsBySearchResponse
+import com.omouravictor.invest_view.data.network.remote.model.assetsbysearch.toAssetsBySearchUiModel
 import com.omouravictor.invest_view.data.network.remote.repository.AssetsRepository
 import com.omouravictor.invest_view.di.base.DispatcherProvider
+import com.omouravictor.invest_view.presenter.base.UiResultState
+import com.omouravictor.invest_view.presenter.wallet.assets.asset_search.model.AssetBySearchUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
@@ -19,25 +22,27 @@ class AssetSearchViewModel @Inject constructor(
     private val dispatchers: DispatcherProvider
 ) : ViewModel() {
 
-    private val _text = MutableLiveData<String>()
-    val text: LiveData<String> = _text
+    private val _assetsBySearch = MutableLiveData<UiResultState<List<AssetBySearchUiModel>>>()
+    val assetsBySearch: LiveData<UiResultState<List<AssetBySearchUiModel>>> = _assetsBySearch
 
     fun getAssetsBySearch(keywords: String) {
         viewModelScope.launch(dispatchers.io) {
             assetsRepository.getRemoteAssetsBySearch(keywords).collectLatest { result ->
                 when (result) {
                     is NetworkResultState.Success -> handleNetworkSuccessResult(result.data)
-                    is NetworkResultState.Error -> handleNetworkErrorResult(result)
+                    is NetworkResultState.Error -> handleNetworkErrorResult(result.e)
                     is NetworkResultState.Loading -> handleNetworkLoadingResult()
                 }
             }
         }
     }
 
-    private fun handleNetworkSuccessResult(data: AssetsBySearchResponse) {
+    private fun handleNetworkSuccessResult(assetsBySearchResponse: AssetsBySearchResponse) {
+        _assetsBySearch.value =
+            UiResultState.Success(assetsBySearchResponse.toAssetsBySearchUiModel())
     }
 
-    private fun handleNetworkErrorResult(message: NetworkResultState<AssetsBySearchResponse>) {
+    private fun handleNetworkErrorResult(e: Exception) {
     }
 
     private fun handleNetworkLoadingResult() {
