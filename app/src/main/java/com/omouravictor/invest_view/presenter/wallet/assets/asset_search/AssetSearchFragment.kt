9d5114090e22ae239386188244058d4f00db5058
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-03-16 20:35:14 -0300

    refactors

--- app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/asset_search/AssetSearchFragment.kt
+++ app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/asset_search/AssetSearchFragment.kt
@@ -1,17 +1,20 @@
 package com.omouravictor.invest_view.presenter.wallet.assets.asset_search
 
+import android.graphics.Color
 import android.os.Bundle
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import androidx.appcompat.widget.SearchView
+import androidx.core.graphics.drawable.toDrawable
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.omouravictor.invest_view.R
 import com.omouravictor.invest_view.databinding.FragmentAssetSearchBinding
@@ -36,43 +39,53 @@ class AssetSearchFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        addSearchOptionMenu()
+        addSearchViewOptionMenu()
         setupRecyclerView()
         observeAssetSearchResults()
     }
 
-    private fun addSearchOptionMenu() {
+    override fun onStop() {
+        super.onStop()
+        SystemServiceUtil.hideKeyboard(requireActivity(), requireView())
+    }
+
+    private fun addSearchViewOptionMenu() {
         requireActivity().addMenuProvider(
             object : MenuProvider {
+
                 override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
                     menuInflater.inflate(R.menu.options_menu_search, menu)
-
                     val searchMenuItem = menu.findItem(R.id.searchAsset)
-                    val searchView = searchMenuItem.actionView as android.widget.SearchView
-
-                    searchView.onActionViewExpanded()
-
-                    searchView.queryHint = getString(R.string.search_your_asset)
-
-                    searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
-                        android.widget.SearchView.OnQueryTextListener {
-                        override fun onQueryTextSubmit(query: String?): Boolean {
-                            query?.let { assetBySearchViewModel.getAssetsBySearch(it) }
-                            SystemServiceUtil.hideKeyboard(requireActivity(), searchView)
-                            return true
-                        }
-
-                        override fun onQueryTextChange(newText: String?): Boolean {
-                            binding.recyclerView.scrollToPosition(0)
-                            return true
-                        }
-                    })
+                    val searchView = searchMenuItem.actionView as SearchView
+                    setupSearchView(searchView)
                 }
 
                 override fun onMenuItemSelected(menuItem: MenuItem): Boolean = false
-            }, viewLifecycleOwner, Lifecycle.State.STARTED
+
+            }, viewLifecycleOwner
         )
+    }
+
+    private fun setupSearchView(searchView: SearchView) {
+        searchView.onActionViewExpanded()
+        searchView.queryHint = getString(R.string.search_your_asset)
+        searchView.inputType = InputType.TYPE_TEXT_FLAG_CAP_CHARACTERS
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                query?.let { assetBySearchViewModel.getAssetsBySearch(it) }
+                SystemServiceUtil.hideKeyboard(requireActivity(), searchView)
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                binding.recyclerView.scrollToPosition(0)
+                return true
+            }
+
+        })
     }
 
     private fun setupRecyclerView() {
