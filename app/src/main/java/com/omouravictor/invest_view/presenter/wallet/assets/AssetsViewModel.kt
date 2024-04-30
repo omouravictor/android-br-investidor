26eaa844bd5862e55f68d3b98e77cdd66a3c9f20
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-04-29 21:42:54 -0300

    remove assets for test

--- app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/AssetsViewModel.kt
+++ app/src/main/java/com/omouravictor/invest_view/presenter/wallet/assets/AssetsViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.omouravictor.invest_view.data.network.remote.repository.RemoteAssetsRepository
 import com.omouravictor.invest_view.di.base.DispatcherProvider
-import com.omouravictor.invest_view.presenter.base.AssetTypes
 import com.omouravictor.invest_view.presenter.wallet.model.AssetUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -23,72 +22,6 @@ class AssetsViewModel @Inject constructor(
     val currentAssetTypes get() = currentAssets.map { it.assetType }.distinct()
     private val _assetsList = MutableLiveData<List<AssetUiModel>>()
     val assetsList: LiveData<List<AssetUiModel>> = _assetsList
-
-    init {
-        val testList = listOf(
-            AssetUiModel(
-                symbol = "AAPL",
-                name = "Apple Inc.",
-                assetType = AssetTypes.STOCK,
-                region = "United States",
-                currency = "USD",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "AAPL34",
-                name = "Apple Inc.",
-                assetType = AssetTypes.LOCAL_DEPOSITARY_RECEIPTS,
-                region = "Brazil",
-                currency = "BRL",
-                price = 50.0,
-                amount = 10,
-                totalInvested = 1000.0
-            ),
-            AssetUiModel(
-                symbol = "MSTFT",
-                name = "Apple Inc.",
-                assetType = AssetTypes.ETF,
-                region = "Germany",
-                currency = "EUR",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "VALE3",
-                name = "Vale S.A.",
-                assetType = AssetTypes.LOCAL_STOCK,
-                region = "Brasil",
-                currency = "BRL",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "MXRF11",
-                name = "Max renda.",
-                assetType = AssetTypes.FI,
-                region = "Brasil",
-                currency = "BRL",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "ASDAS",
-                name = "Vale S.A.",
-                assetType = AssetTypes.OTHER,
-                region = "Brasil",
-                currency = "BRL",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            )
-        )
-        _assetsList.value = testList
-    }
 
     fun addAsset(assetUiModel: AssetUiModel) {
         val currentList = currentAssets.toMutableList()
