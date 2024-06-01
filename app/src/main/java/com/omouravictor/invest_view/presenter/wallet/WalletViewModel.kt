f26b054a306587d41fbc11a8ad7d3361c00dcb8b
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-05-31 21:54:56 -0300

    refactors

--- app/src/main/java/com/omouravictor/invest_view/presenter/wallet/WalletViewModel.kt
+++ app/src/main/java/com/omouravictor/invest_view/presenter/wallet/WalletViewModel.kt
@@ -1,23 +1,28 @@
 package com.omouravictor.invest_view.presenter.wallet
 
-import android.content.Context
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.omouravictor.invest_view.data.network.remote.repository.RemoteAssetsRepository
+import androidx.lifecycle.viewModelScope
+import com.omouravictor.invest_view.data.network.base.NetworkState
+import com.omouravictor.invest_view.data.network.remote.repository.RemoteDatabaseRepository
 import com.omouravictor.invest_view.di.base.DispatcherProvider
-import com.omouravictor.invest_view.presenter.wallet.asset_types.AssetTypes
+import com.omouravictor.invest_view.presenter.base.UiState
 import com.omouravictor.invest_view.presenter.wallet.model.AssetUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
 class WalletViewModel @Inject constructor(
-    @ApplicationContext private val context: Context,
-    private val remoteAssetsRepository: RemoteAssetsRepository,
+    private val remoteDatabaseRepository: RemoteDatabaseRepository,
     private val dispatchers: DispatcherProvider
 ) : ViewModel() {
+
+    private val _walletUiState = MutableLiveData<UiState<Nothing>>()
+    val walletUiState: LiveData<UiState<Nothing>> = _walletUiState
 
     private val _assetsLiveData = MutableLiveData<List<AssetUiModel>>()
     val assetsLiveData: LiveData<List<AssetUiModel>> = _assetsLiveData
@@ -25,116 +30,24 @@ class WalletViewModel @Inject constructor(
     val assetTypesList get() = assetsList.map { it.assetType }.distinct()
     val assetCurrenciesList get() = assetsList.map { it.currency }.distinct()
 
-    init {
-        val testList = listOf(
-            AssetUiModel(
-                symbol = "AAPL",
-                name = "Apple Inc.",
-                assetType = AssetTypes.STOCK,
-                region = "United States",
-                currency = "USD",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "AAPL34",
-                name = "Apple Inc.",
-                assetType = AssetTypes.BDR,
-                region = "Brazil/Sao Paolo",
-                currency = "BRL",
-                price = 50.0,
-                amount = 10,
-                totalInvested = 1000.0
-            ),
-            AssetUiModel(
-                symbol = "MSFT",
-                name = "Apple Inc.",
-                assetType = AssetTypes.ETF,
-                region = "Amsterdam",
-                currency = "EUR",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "VALE3",
-                name = "Vale S.A.",
-                assetType = AssetTypes.BRAZILIAN_STOCK,
-                region = "Brazil/Sao Paolo",
-                currency = "CNY",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "MXRF11",
-                name = "Max renda.",
-                assetType = AssetTypes.FI,
-                region = "Brazil/Sao Paolo",
-                currency = "INR",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "ASDAS",
-                name = "Vale S.A.",
-                assetType = AssetTypes.OTHER,
-                region = "Brazil/Sao Paolo",
-                currency = "CAD",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "AAPL34",
-                name = "Apple Inc.",
-                assetType = AssetTypes.BDR,
-                region = "Brazil/Sao Paolo",
-                currency = "BRL",
-                price = 50.0,
-                amount = 10,
-                totalInvested = 1000.0
-            ),
-            AssetUiModel(
-                symbol = "MSFT",
-                name = "Apple Inc.",
-                assetType = AssetTypes.ETF,
-                region = "Amsterdam",
-                currency = "EUR",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "VALE3",
-                name = "Vale S.A.",
-                assetType = AssetTypes.BRAZILIAN_STOCK,
-                region = "Brazil/Sao Paolo",
-                currency = "CNY",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-            AssetUiModel(
-                symbol = "MXRF11",
-                name = "Max renda.",
-                assetType = AssetTypes.FI,
-                region = "Brazil/Sao Paolo",
-                currency = "INR",
-                price = 150.0,
-                amount = 10,
-                totalInvested = 1600.0
-            ),
-        )
-        _assetsLiveData.value = testList
-    }
+    fun saveAsset(asset: AssetUiModel) {
+        viewModelScope.launch {
+            withContext(dispatchers.io) {
+                remoteDatabaseRepository.save(asset)
+            }.collectLatest {
+                when (it) {
+                    is NetworkState.Loading -> _walletUiState.value = UiState.Loading
 
-    fun addAsset(assetUiModel: AssetUiModel) {
-        val currentList = assetsList.toMutableList()
-        currentList.add(assetUiModel)
-        _assetsLiveData.value = currentList
+                    is NetworkState.Success -> {
+                        val currentList = assetsList.toMutableList()
+                        currentList.add(it.data)
+                        _assetsLiveData.value = currentList
+                    }
+
+                    is NetworkState.Error -> _walletUiState.value = UiState.Error(it.e)
+                }
+            }
+        }
     }
 
 }
