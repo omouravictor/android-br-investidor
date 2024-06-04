1b386561306a1be476b27af12134383657569ef4
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-06-03 21:27:03 -0300

    refactors

--- app/src/main/java/com/omouravictor/invest_view/data/network/remote/repository/FirebaseRepository.kt
+++ app/src/main/java/com/omouravictor/invest_view/data/network/remote/repository/FirebaseRepository.kt
@@ -3,5 +3,6 @@ package com.omouravictor.invest_view.data.network.remote.repository
 import com.omouravictor.invest_view.presenter.wallet.model.AssetUiModel
 
 interface FirebaseRepository {
-    fun saveAsset(assetUiModel: AssetUiModel): Result<AssetUiModel>
+    suspend fun saveAsset(assetUiModel: AssetUiModel): Result<AssetUiModel>
+    suspend fun getAssetsList(): Result<List<AssetUiModel>>
 }
--- app/src/main/java/com/omouravictor/invest_view/data/network/remote/repository/FirebaseRepositoryImpl.kt
+++ app/src/main/java/com/omouravictor/invest_view/data/network/remote/repository/FirebaseRepositoryImpl.kt
@@ -3,22 +3,37 @@ package com.omouravictor.invest_view.data.network.remote.repository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.omouravictor.invest_view.presenter.wallet.model.AssetUiModel
+import kotlinx.coroutines.tasks.await
 
 class FirebaseRepositoryImpl(
     private val auth: FirebaseAuth,
     private val firestore: FirebaseFirestore
 ) : FirebaseRepository {
 
-    override fun saveAsset(assetUiModel: AssetUiModel): Result<AssetUiModel> {
+    override suspend fun saveAsset(assetUiModel: AssetUiModel): Result<AssetUiModel> {
         return try {
             val userId = auth.currentUser?.uid ?: throw Exception("Usuário não autenticado.")
 
             firestore.collection("users").document(userId)
                 .collection("assets").document(assetUiModel.symbol)
-                .set(assetUiModel)
-                .addOnFailureListener { throw it }
+                .set(assetUiModel).await()
 
             Result.success(assetUiModel)
+
+        } catch (e: Exception) {
+            Result.failure(e)
+        }
+    }
+
+    override suspend fun getAssetsList(): Result<List<AssetUiModel>> {
+        return try {
+            val userId = auth.currentUser?.uid ?: throw Exception("Usuário não autenticado.")
+
+            val assetsList = firestore.collection("users").document(userId)
+                .collection("assets").get().await()
+                .toObjects(AssetUiModel::class.java)
+
+            Result.success(assetsList)
 
         } catch (e: Exception) {
             Result.failure(e)
