635a4d0dd5cb5f5a54fed8ab8e8c4f0068c5288a
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-09-09 18:03:22 -0300

    refactors

--- app/src/main/java/com/omouravictor/invest_view/util/LocaleUtil.kt
+++ app/src/main/java/com/omouravictor/invest_view/util/LocaleUtil.kt
@@ -4,14 +4,11 @@ import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.Currency
 import java.util.Locale
-import java.util.TimeZone
 
 object LocaleUtil {
 
     val appLocale = Locale("pt", "BR")
-    private val appTimeZone = TimeZone.getTimeZone("America/Sao_Paulo")
-    private val appDateTimeFormat = SimpleDateFormat("dd/MM/yyyy HH:mm", appLocale)
-        .apply { timeZone = appTimeZone }
+    private val appDateFormat = SimpleDateFormat("dd/MM/yyyy", appLocale)
     private val isoDateTimeFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", appLocale)
 
     fun getFormattedCurrencyValue(currency: String, value: Number): String {
@@ -35,7 +32,7 @@ object LocaleUtil {
         if (dateTimeString.isNullOrEmpty()) return ""
 
         val parsedDate = isoDateTimeFormat.parse(dateTimeString) ?: return ""
-        return appDateTimeFormat.format(parsedDate)
+        return appDateFormat.format(parsedDate)
     }
 
 }
