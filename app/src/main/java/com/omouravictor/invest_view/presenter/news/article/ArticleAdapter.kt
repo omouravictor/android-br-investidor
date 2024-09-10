65b8f50b98b28539fb82e520dd428f1515b0f7f2
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-09-09 21:00:59 -0300

    refactors

--- app/src/main/java/com/omouravictor/invest_view/presenter/news/article/ArticleAdapter.kt
+++ app/src/main/java/com/omouravictor/invest_view/presenter/news/article/ArticleAdapter.kt
@@ -1,12 +1,12 @@
 package com.omouravictor.invest_view.presenter.news.article
 
-import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.omouravictor.invest_view.databinding.ItemNewsBinding
 import com.omouravictor.invest_view.presenter.base.BaseRecyclerViewAdapter
 import com.omouravictor.invest_view.presenter.model.ArticleUiModel
@@ -14,6 +14,9 @@ import com.omouravictor.invest_view.util.LocaleUtil
 
 
 class ArticleAdapter : BaseRecyclerViewAdapter<ArticleUiModel, ArticleAdapter.NewsBySearchViewHolder>() {
+
+    private val glideRequestOptions = RequestOptions()
+        .transform(CenterCrop(), RoundedCorners(24))
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsBySearchViewHolder {
         val binding = ItemNewsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -27,12 +30,11 @@ class ArticleAdapter : BaseRecyclerViewAdapter<ArticleUiModel, ArticleAdapter.Ne
     inner class NewsBySearchViewHolder(private val binding: ItemNewsBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        @SuppressLint("SetTextI18n")
         fun bind(articleUiModel: ArticleUiModel) {
             binding.apply {
                 Glide.with(itemView)
                     .load(articleUiModel.urlToImage)
-                    .transform(CenterCrop(), RoundedCorners(24))
+                    .apply(glideRequestOptions)
                     .into(articleImage)
                 articleSource.text = articleUiModel.source.name
                 articleTitle.text = articleUiModel.title
