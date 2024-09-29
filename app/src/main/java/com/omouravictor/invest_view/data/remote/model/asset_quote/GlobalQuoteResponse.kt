0c98924f7fbb3b3703f2bd8ea405ed02facdf087
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-09-29 15:24:23 -0300

    refactors

--- app/src/main/java/com/omouravictor/invest_view/data/remote/model/asset_quote/GlobalQuoteResponse.kt
+++ app/src/main/java/com/omouravictor/invest_view/data/remote/model/asset_quote/GlobalQuoteResponse.kt
@@ -4,13 +4,13 @@ import com.google.gson.annotations.SerializedName
 
 data class GlobalQuoteResponse(
     @SerializedName("01. symbol") val symbol: String = "",
-    @SerializedName("02. open") val open: Double = 0.0,
-    @SerializedName("03. high") val high: Double = 0.0,
-    @SerializedName("04. low") val low: Double = 0.0,
-    @SerializedName("05. price") val price: Double = 0.0,
-    @SerializedName("06. volume") val volume: Int = 0,
+    @SerializedName("02. open") val open: Double = -1.0,
+    @SerializedName("03. high") val high: Double = -1.0,
+    @SerializedName("04. low") val low: Double = -1.0,
+    @SerializedName("05. price") val price: Double = -1.0,
+    @SerializedName("06. volume") val volume: Int = -1,
     @SerializedName("07. latest trading day") val latestTradingDay: String = "",
-    @SerializedName("08. previous close") val previousClose: Double = 0.0,
-    @SerializedName("09. change") val change: Double = 0.0,
+    @SerializedName("08. previous close") val previousClose: Double = -1.0,
+    @SerializedName("09. change") val change: Double = -1.0,
     @SerializedName("10. change percent") val changePercent: String = ""
 )
