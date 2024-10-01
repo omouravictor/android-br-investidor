5ab06319f1923397771d318b2d71e7ba4269b0e7
omouravictor <victor.moura@aluno.ufop.edu.br>
2024-09-30 21:35:14 -0300

    refactors

--- app/src/main/java/com/omouravictor/invest_view/data/remote/model/asset_quote/AssetGlobalQuoteResponse.kt
+++ app/src/main/java/com/omouravictor/invest_view/data/remote/model/asset_quote/AssetGlobalQuoteResponse.kt
@@ -2,6 +2,7 @@ package com.omouravictor.invest_view.data.remote.model.asset_quote
 
 import com.google.gson.annotations.SerializedName
 import com.omouravictor.invest_view.presenter.wallet.model.GlobalQuoteUiModel
+import com.omouravictor.invest_view.util.getRoundedDouble
 
 data class AssetGlobalQuoteResponse(
     @SerializedName("Global Quote") val globalQuoteResponse: GlobalQuoteResponse
@@ -10,8 +11,8 @@ data class AssetGlobalQuoteResponse(
 fun AssetGlobalQuoteResponse.toGlobalQuoteUiModel(): GlobalQuoteUiModel {
     return GlobalQuoteUiModel(
         symbol = globalQuoteResponse.symbol,
-        price = globalQuoteResponse.price,
-        change = globalQuoteResponse.change,
-        changePercent = globalQuoteResponse.changePercent.removeSuffix("%").toDoubleOrNull() ?: 0.0
+        price = globalQuoteResponse.price.getRoundedDouble(),
+        change = globalQuoteResponse.change.getRoundedDouble(),
+        changePercent = globalQuoteResponse.changePercent.removeSuffix("%").toDoubleOrNull()?.getRoundedDouble() ?: 0.0
     )
 }
